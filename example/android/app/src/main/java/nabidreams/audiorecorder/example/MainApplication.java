0c572c2645b09eafc1eaede6e1f2fa9cb7b8b0ba
krazijames <krazijames@gmail.com>
2019-11-08 03:43:32 +0900

    Move example app package

--- example/android/app/src/main/java/nabidreams/audiorecorder/example/MainApplication.java
+++ example/android/app/src/main/java/nabidreams/audiorecorder/example/MainApplication.java
@@ -1,7 +1,8 @@
-package com.audiorecorderexample;
+package nabidreams.audiorecorder.example;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
