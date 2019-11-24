185dca22d6d5717e9d3d29c86401cef9678a555b
krazijames <krazijames@gmail.com>
2019-11-24 19:15:05 +0900

    Remove unnecessary UI

--- example/App.js
+++ example/App.js
@@ -1,38 +1,16 @@
 import React from 'react';
-import {
-  PermissionsAndroid,
-  StyleSheet,
-  Text,
-  View,
-  Button,
-} from 'react-native';
-import Audio, { Recorder, Player } from '@nabidreams/react-native-audio';
+import { PermissionsAndroid, StyleSheet, View, Button } from 'react-native';
+import { Recorder, Player } from '@nabidreams/react-native-audio';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
 export default function App() {
-  const [state, setState] = React.useState({
-    status: 'starting',
-    message: '--',
-  });
-
   const [
     recordAudioPermissionGranted,
     setRecordAudioPermissionGranted,
@@ -83,26 +61,12 @@ export default function App() {
     }
   }
 
-  React.useEffect(function testNativeModule() {
-    Audio.sampleMethod('Testing', 123, (message) => {
-      setState({
-        status: 'native callback received',
-        message,
-      });
-    });
-  }, []);
-
   React.useEffect(function requestPermission() {
     requestRecordAudioPermission();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>☆Audio example☆</Text>
-      <Text style={styles.instructions}>STATUS: {state.status}</Text>
-      <Text style={styles.welcome}>☆NATIVE CALLBACK MESSAGE☆</Text>
-      <Text style={styles.instructions}>{state.message}</Text>
-
       {!recordAudioPermissionGranted && (
         <Button
           title="Request Record Audio Permission"
