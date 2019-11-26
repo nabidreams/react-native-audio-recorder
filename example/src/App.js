a0eb0396c77ea9da29dca5cef7ece3f0904000c7
krazijames <krazijames@gmail.com>
2019-11-27 02:22:49 +0900

    Request permission

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform, StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
 
 import usePermission from './usePermission';
 import RecorderExample from './RecorderExample';
@@ -8,6 +8,12 @@ import PlayerExample from './PlayerExample';
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+    alignItems: 'stretch',
+  },
+  permissionText: {
+    textAlign: 'center',
+    margin: 8,
+    fontWeight: '700',
   },
   container: {
     flex: 1,
@@ -23,23 +29,16 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.root}>
-      {Platform.OS === 'android' && (
-        <Button
-          title={
-            permissionGranted
-              ? 'Record Audio Permission Granted!'
-              : 'Request Record Audio Permission'
-          }
-          onPress={requestPermission}
-          disabled={permissionGranted}
-        />
-      )}
-
+    <SafeAreaView style={styles.root}>
+      <Text style={styles.permissionText}>
+        {permissionGranted
+          ? 'Audio Permission Granted :D'
+          : 'Audio Permission Required ;('}
+      </Text>
       <View style={styles.container}>
         <RecorderExample disabled={!permissionGranted} />
         <PlayerExample disabled={!permissionGranted} />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
--- example/src/usePermission.js
+++ example/src/usePermission.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import { PermissionsAndroid, Platform } from 'react-native';
+import { Platform } from 'react-native';
+import { request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+
+function getPermission() {
+  switch (Platform.OS) {
+    case 'android':
+      return PERMISSIONS.ANDROID.RECORD_AUDIO;
+    case 'ios':
+      return PERMISSIONS.IOS.MICROPHONE;
+    default:
+      return;
+  }
+}
 
 export default () => {
   const [permissionGranted, setPermissionGranted] = React.useState();
 
   const requestPermission = React.useCallback(async () => {
     try {
-      if (Platform.OS !== 'android') {
+      const permission = getPermission();
+
+      if (!permission) {
         return;
       }
 
-      const result = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-      );
+      const result = await request(permission);
 
-      setPermissionGranted(result === PermissionsAndroid.RESULTS.GRANTED);
+      setPermissionGranted(result === RESULTS.GRANTED);
     } catch (err) {
-      console.warn(err);
+      console.error(err);
     }
   }, []);
 
