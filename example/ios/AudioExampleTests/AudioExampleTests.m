8ae736b7fcb658b81d48043737413798075b1b3a
krazijames <krazijames@gmail.com>
2020-04-02 21:15:23 +0900

    Update example test

--- example/ios/AudioExampleTests/AudioExampleTests.m
+++ example/ios/AudioExampleTests/AudioExampleTests.m
@@ -4,33 +4,18 @@
 #import <React/RCTLog.h>
 #import <React/RCTRootView.h>
 
-#define TIMEOUT_SECONDS 600
-#define TEXT_TO_LOOK_FOR @"Welcome to React"
+#define TIMEOUT_SECONDS 10
 
-@interface exampleTests : XCTestCase
+@interface AudioExampleTests : XCTestCase
 
 @end
 
-@implementation exampleTests
+@implementation AudioExampleTests
 
-- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
-{
-  if (test(view)) {
-    return YES;
-  }
-  for (UIView *subview in [view subviews]) {
-    if ([self findSubviewInView:subview matching:test]) {
-      return YES;
-    }
-  }
-  return NO;
-}
-
-- (void)testRendersWelcomeScreen
+- (void)testRendersWithoutError
 {
   UIViewController *vc = [[[RCTSharedApplication() delegate] window] rootViewController];
   NSDate *date = [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_SECONDS];
-  BOOL foundElement = NO;
 
   __block NSString *redboxError = nil;
 #ifdef DEBUG
@@ -41,16 +26,9 @@
   });
 #endif
 
-  while ([date timeIntervalSinceNow] > 0 && !foundElement && !redboxError) {
+  while ([date timeIntervalSinceNow] > 0 && !redboxError) {
     [[NSRunLoop mainRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
     [[NSRunLoop mainRunLoop] runMode:NSRunLoopCommonModes beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-
-    foundElement = [self findSubviewInView:vc.view matching:^BOOL(UIView *view) {
-      if ([view.accessibilityLabel isEqualToString:TEXT_TO_LOOK_FOR]) {
-        return YES;
-      }
-      return NO;
-    }];
   }
 
 #ifdef DEBUG
@@ -58,7 +36,6 @@
 #endif
 
   XCTAssertNil(redboxError, @"RedBox error: %@", redboxError);
-  XCTAssertTrue(foundElement, @"Couldn't find element with text '%@' in %d seconds", TEXT_TO_LOOK_FOR, TIMEOUT_SECONDS);
 }
 
 
